857682b5edd6aee82ab96d195bf779cd387c5616
andrea lee <lee.ays.lee@gmail.com>
2017-12-17 20:50:07 -0500

    removed paperclip from environments due to error on migration, added query(?) to pen Model

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -53,4 +53,7 @@ Rails.application.configure do
   config.file_watcher = ActiveSupport::EventedFileUpdateChecker
   #Added (Devise Documentation-setup)
   config.action_mailer.default_url_options = { host: 'localhost', port: 3000 }
+  #added for paperclip
+  # Paperclip.options[command_path] = "/usr/local/bin"
+
 end
