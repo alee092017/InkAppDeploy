bee3af62463f4a27cb36c7bc89370678e8302159
andrea lee <lee.ays.lee@gmail.com>
2017-12-18 01:58:52 -0500

    RATING was not displaying in show b/c it was not part of the permitted params in controller :/  also added some minor styling just for kicks

--- app/controllers/pens_controller.rb
+++ app/controllers/pens_controller.rb
@@ -70,6 +70,6 @@ class PensController < ApplicationController
 
     # Never trust parameters from the scary internet, only allow the white list through.
     def pen_params
-      params.require(:pen).permit(:name, :description, :tip_size, :ink_color, :model_no, :image_url, :image)
+      params.require(:pen).permit(:name, :description, :tip_size, :ink_color, :model_no, :image_url, :image, :rating)
     end
 end
